cb638757c583dee9df34ad4a834b132c3942c8fc
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-02-06 15:29:05 -0300

    Serializando um objeto com Gson.

--- ecommerce/src/main/java/br/com/kaspper/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/kaspper/ecommerce/NewOrderMain.java
@@ -1,19 +1,25 @@
 package br.com.kaspper.ecommerce;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (var i = 0; i < 10; i++) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
 
-                var key = UUID.randomUUID().toString();
-                var value = key + ",67523,1234";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
---
+++ ecommerce/src/main/java/br/com/kaspper/ecommerce/Order.java
@@ -0,0 +1,35 @@
+package br.com.kaspper.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+    private String userId, orderId;
+    private final BigDecimal amount;
+
+    public Order(String userId, String orderId, BigDecimal amount) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+}
