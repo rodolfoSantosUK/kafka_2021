7c21c4ab07279ab5c443b4e613f468373cb89fcc
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-13 22:09:16 -0300

    Acerto no codigo

--- common-kafka/src/main/java/br/com/kaspper/ecommerce/ConsumerFunction.java
+++
@@ -1,9 +0,0 @@
-package br.com.kaspper.ecommerce;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-import java.util.concurrent.ExecutionException;
-
-public interface ConsumerFunction<T> {
-    void consume(ConsumerRecord<String, T> record) throws ExecutionException, InterruptedException;
-}
